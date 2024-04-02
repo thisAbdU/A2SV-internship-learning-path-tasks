78ee9a823f2b47a3bd75baf7573a9f858cb5039b
Abdu <abdulfetaheishak@gmail.com>
2024-04-02 10:00:42 -0400

    do login and register user

--- task-management-api/middleware/auth.go
+++ task-management-api/middleware/auth.go
@@ -1,66 +1,39 @@
 package middleware
 
 import (
-	"net/http"
-	"time"
-
-	"github.com/gin-gonic/gin"
-	"github.com/golang-jwt/jwt"
+   
+    "time"
+    "github.com/dgrijalva/jwt-go"
 )
 
-var (
-    jwtKey = []byte("your-secret-key")
-)
-
+var jwtKey = []byte("your-secret-key")
 
 type Claims struct {
     UserID string `json:"userID"`
     jwt.StandardClaims
 }
 
-
-func GenerateToken(userID string) (string, error) {
+func GenerateToken(s string) (string, error) {
+    // Set expiration time for the token (24 hours in this example)
     expirationTime := time.Now().Add(24 * time.Hour)
+
+    // Create custom claims
     claims := &Claims{
-        UserID: userID,
+        UserID: s,
         StandardClaims: jwt.StandardClaims{
             ExpiresAt: expirationTime.Unix(),
+            IssuedAt:  time.Now().Unix(),
         },
     }
+
+    // Create a token with the claims
     token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-    return token.SignedString(jwtKey)
-}
 
-
-func VerifyToken(c *gin.Context) (*Claims, error) {
-    tokenString := extractTokenFromHeader(c)
-    token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
-        return jwtKey, nil
-    })
+    // Sign the token with a secret key
+    tokenString, err := token.SignedString(jwtKey)
     if err != nil {
-        return nil, err
+        return "", err
     }
-    claims, ok := token.Claims.(*Claims)
-    if !ok || !token.Valid {
-        return nil, jwt.ErrSignatureInvalid
-    }
-    return claims, nil
-}
 
-func Middleware() gin.HandlerFunc {
-    return func(c *gin.Context) {
-        _, err := VerifyToken(c)
-        if err != nil {
-            c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
-            c.Abort()
-            return
-        }
-        c.Next()
-    }
-}
-
-
-func extractTokenFromHeader(c *gin.Context) string {
-    bearerToken := c.GetHeader("Authorization")
-    return bearerToken
+    return tokenString, nil
 }
